2bb9da8cf4f6918bc5111e58323a91d15db5b044
Wei Chen <tengwei1@gmail.com>
2017-08-07 17:26:21 -0400

    added click listener to cards

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+  private clicked: boolean;
+  
   constructor() { }
 
   ngOnInit() {
+    this.clicked = false;
   }
-
+  
+  onClick() {
+    this.clicked = true;
+    setTimeout(() => {
+      this.clicked = false;
+    }, 2000);
+  }
 }

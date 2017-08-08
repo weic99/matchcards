7e33069ff96024b962f3a8fbde409ac399f91aed
Wei Chen <tengwei1@gmail.com>
2017-08-08 13:56:34 -0400

    card click event now binded to game board

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() card: any;
+  @Output() onSelected = new EventEmitter<any>();
+  
+  private imageUrl: string = "https://firebasestorage.googleapis.com/v0/b/matchcards-7d5da.appspot.com/o/pokemon%2FQuestionMark.png?alt=media&token=ad883b8a-b08c-4c7d-be32-0ba6fe9459ac";
+  
   private clicked: boolean;
   
   constructor() { }
@@ -14,10 +19,12 @@ export class CardComponent implements OnInit {
     this.clicked = false;
   }
   
-  onClick() {
+  onClick(clicked: boolean) {
     this.clicked = true;
     setTimeout(() => {
       this.clicked = false;
     }, 2000);
+    
+    this.onSelected.emit(this.card);
   }
 }
--- client/src/app/components/matchcardsboard/matchcardsboard.component.ts
+++ client/src/app/components/matchcardsboard/matchcardsboard.component.ts
@@ -7,12 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchcardsboardComponent implements OnInit {
   private cards: any[];
+  private cardCount: number;
+  private turn: number;
   
   constructor() { }
 
   ngOnInit() {
-    this.cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    this.cardCount = 12;
+    this.cards = [];
+    for (let i = 0; i < this.cardCount; i++) {
+      this.cards.push(i);
+      this.cards.push(i);
+    }
+    
+    this.shuffle(this.cards);
   }
   
+  private shuffle(array) {
+    let currentIndex = array.length;
+    let temp;
+    let randomIndex;
+    
+    while (currentIndex-- !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      temp = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
+  }
+  
+  private onSelected(card: any) {
+    console.log(card);
+  }
 }

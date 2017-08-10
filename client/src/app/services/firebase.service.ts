2d4e2c02994dbfd12388ad0e4ebf8da71c461adc
Wei Chen <tengwei1@gmail.com>
2017-08-10 16:08:46 -0400

    "" to ''

--- client/src/app/services/firebase.service.ts
+++ client/src/app/services/firebase.service.ts
@@ -7,12 +7,12 @@ export class FirebaseService {
   constructor() { 
     // Initialize Firebase
     const config = {
-      apiKey: "AIzaSyA80FA_OFFdhaYEhVxjM0eNDBR6HGYdtB4",
-      authDomain: "matchcards-7d5da.firebaseapp.com",
-      databaseURL: "https://matchcards-7d5da.firebaseio.com",
-      projectId: "matchcards-7d5da",
-      storageBucket: "matchcards-7d5da.appspot.com",
-      messagingSenderId: "193741764493"
+      apiKey: 'AIzaSyA80FA_OFFdhaYEhVxjM0eNDBR6HGYdtB4',
+      authDomain: 'matchcards-7d5da.firebaseapp.com',
+      databaseURL: 'https://matchcards-7d5da.firebaseio.com',
+      projectId: 'matchcards-7d5da',
+      storageBucket: 'matchcards-7d5da.appspot.com',
+      messagingSenderId: '193741764493'
     };
     firebase.initializeApp(config);
   }

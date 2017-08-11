7e78ca1c68310d70a6037aa7a682c092e1148fcd
Wei Chen <tengwei1@gmail.com>
2017-08-11 18:52:07 -0400

    refractor

--- client/src/app/directives/ng-scroll.directive.ts
+++ client/src/app/directives/ng-scroll.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[ngScroll]'
 })
 export class NgScrollDirective {
+  @Input('ngScroll') offset: number; /** % offset from the bottom */
+
   @HostListener('scroll', ['$event'])
   onScroll(event) {
     let el = event.target;
-    let limit = el.scrollHeight - el.clientHeight;
+    let limit = (el.scrollHeight - el.clientHeight) * (100 - this.offset) / 100;
 
-    if (event.target.scrollTop === limit) {
+    /** reached the target percentage */
+    if (limit <= event.target.scrollTop) {
       console.log('end');
     }
   };
-
+  
   constructor() { }
 }

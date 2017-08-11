1f1b452172ba9cf367692a18bba19999d7d2af8a
Wei Chen <tengwei1@gmail.com>
2017-08-11 19:25:55 -0400

    now hides scroll indicators when scroll to bottom

--- client/src/app/directives/ng-scroll.directive.ts
+++ client/src/app/directives/ng-scroll.directive.ts
@@ -4,18 +4,22 @@ import { Directive, HostListener, Input } from '@angular/core';
   selector: '[ngScroll]'
 })
 export class NgScrollDirective {
-  @Input('ngScroll') offset: number; /** % offset from the bottom */
+  @Input('ngScroll') offset: number; /** offset px from the bottom */
 
   @HostListener('scroll', ['$event'])
   onScroll(event) {
     let el = event.target;
-    let limit = (el.scrollHeight - el.clientHeight) * (100 - this.offset) / 100;
+    let limit = (el.scrollHeight - el.clientHeight)  - this.offset;//* (100 - this.offset) / 100;
 
     /** reached the target percentage */
     if (limit <= event.target.scrollTop) {
-      console.log('end');
+      document.getElementById('scroll').style.display = 'none'; /** hide the scroll indicators */
+      document.getElementById('scroll2').style.display = 'none';
+    } else {
+      document.getElementById('scroll').style.display = 'inline'; /** show the scroll indicators */
+      document.getElementById('scroll2').style.display = 'inline';
     }
   };
-  
+
   constructor() { }
 }
